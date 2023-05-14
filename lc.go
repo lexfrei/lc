8e3ccef695f6eb87037b5d1564fa825533edd202
Aleksey Sviridkin <a.sviridkin@sdventures.com>
2023-05-14 19:16:34 +0300

    add romanToInt
    Signed-off-by: Aleksey Sviridkin <a.sviridkin@sdventures.com>

--- lc.go
+++ lc.go
@@ -47,3 +47,42 @@ func isPalindrome(x int) bool {
 
 	return true
 }
+
+// romanToInt converts a roman numeral to an integer.
+// Problem number: 13
+// URL: https://leetcode.com/problems/roman-to-integer/
+//
+// I	1
+// V	5
+// X	10
+// L	50
+// C	100
+// D	500
+// M	1000
+//
+// I can be placed before V (5) and X (10) to make 4 and 9.
+// X can be placed before L (50) and C (100) to make 40 and 90.
+// C can be placed before D (500) and M (1000) to make 400 and 900.
+func romanToInt(s string) int {
+	romanToIntMap := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	var result int
+
+	for i := 0; i < len(s); i++ {
+		if i < len(s)-1 && romanToIntMap[s[i]] < romanToIntMap[s[i+1]] {
+			result -= romanToIntMap[s[i]]
+		} else {
+			result += romanToIntMap[s[i]]
+		}
+	}
+
+	return result
+}
--- lc_test.go
+++ lc_test.go
@@ -103,3 +103,43 @@ func Test_isPalindrome(t *testing.T) {
 		})
 	}
 }
+
+func Test_romanToInt(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				s: "III",
+			},
+			want: 3,
+		},
+		{
+			name: "test2",
+			args: args{
+				s: "IV",
+			},
+			want: 4,
+		},
+		{
+			name: "test3",
+			args: args{
+				s: "MCMXCIV",
+			},
+			want: 1994,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := romanToInt(tt.args.s); got != tt.want {
+				t.Errorf("romanToInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
